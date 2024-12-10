1582c0b44c6fabfcbcdd990d10003133e573448c
omar <omarbarhoumy229@gmail.com>
2024-12-10 18:00:15 +0100

    first commit

---
+++ product-display/src/App.js
@@ -0,0 +1,49 @@
+// src/App.js
+import React from 'react';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+import Name from './components/Name';
+import Price from './components/Price';
+import Description from './components/Description';
+import Image from './components/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import userImage from './assets/user.jpg';  // Import the image
+
+const App = () => {
+  const userName = "omar"; // Remplacez par votre prénom ou laissez vide
+  const greeting = userName ? `Bonjour, ${userName}` : 'Bonjour !';
+
+  return (
+    <Container className="mt-5">
+      <Row className="justify-content-center">  
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+               <Image />
+              <Card.Title><Name /></Card.Title>
+              <Card.Text>
+                <Price />
+                <Description />
+              </Card.Text>
+            </Card.Body>
+            
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="text-center">
+          <h5>{greeting}</h5>
+          {userName && <img src={userImage} alt="Product"  />}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default App;
+
+
+
+
+
+
---
+++ product-display/src/components/product.js
@@ -0,0 +1,10 @@
+// src/product.js
+const product = {
+    name: "Samsung S24 ULTRA",
+    price: 500.000,
+    description: "Un smartphone haut de gamme avec un écran OLED et un processeur ultra-rapide.",
+    image: "./assets/product.jpg"
+  };
+  
+  export default product;
+  
